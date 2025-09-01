b136d67de019e00dbc2ccd12e09894e5ea014ac1
Nguyễn Phước Hảo <nguyenphuochao123@gmail.com>
2025-09-01 23:18:32 +0700

    feat(login): login & logout & use httponly cookie security

--- laravel-admin/app/Http/Controllers/AuthController.php
+++ laravel-admin/app/Http/Controllers/AuthController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
-    function register(Request $request)
+    function register(RegisterRequest $request)
     {
         $user = User::create([
             "first_name" => $request->input("first_name"),
@@ -20,5 +21,41 @@ class AuthController extends Controller
         ]);
 
         return response($user, Response::HTTP_CREATED);
+    }
+
+    function login(Request $request)
+    {
+        if (!Auth::attempt($request->only("email", "password"))) {
+            return \response([
+                "error" => "Invalid credentials"
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = Auth::user();
+
+        $jwt = $user->createToken("token")->plainTextToken;
+
+        // FE không giữ token, trình duyệt giữ hộ trong cookie và tự gửi kèm khi gọi API.
+        // Nếu lưu token trong localStorage hoặc sessionStorage:
+        // Dễ bị XSS (nếu hacker inject script thì đọc được token).
+        $cookie = cookie("jwt", $jwt, 60 * 24); // 1 day
+
+        return response([
+            "jwt" => $jwt
+        ])->withCookie($cookie);
+    }
+
+    function user(Request $request)
+    {
+        return $request->user();
+    }
+
+    function logout()
+    {
+        $cookie = \Cookie::forget("jwt");
+
+        return \response([
+            "message" => "success"
+        ])->withCookie($cookie);
     }
 }
--- laravel-admin/routes/api.php
+++ laravel-admin/routes/api.php
@@ -17,5 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post("register",[AuthController::class, "register"]);
+Route::post("register", [AuthController::class, "register"]);
+Route::post("login", [AuthController::class, "login"]);
 
+Route::middleware("auth:sanctum")->group(function () {
+    Route::get("user", [AuthController::class, "user"]);
+    Route::post("logout", [AuthController::class, "logout"]);
+});
