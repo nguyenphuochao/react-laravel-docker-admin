c511dfd20e1e8eace99ef1a13010e5f24794d68c
Nguyễn Phước Hảo <nguyenphuochao123@gmail.com>
2025-09-02 23:51:40 +0700

    feat(permissions): add route & function

--- laravel-admin/routes/api.php
+++ laravel-admin/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -26,4 +28,6 @@ Route::middleware("auth:sanctum")->group(function () {
     Route::put("users/password", [AuthController::class, "updatePassword"]);
 
     Route::apiResource("users", UserController::class);
+    Route::apiResource("roles", RoleController::class);
+    Route::get("permissions", [PermissionController::class, "index"]);
 });
