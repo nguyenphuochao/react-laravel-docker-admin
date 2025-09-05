37ea5fbc455e93647f5aeabf5046024cdac70554
NhungNTT <nhungntt@splus-software.com.vn>
2025-09-05 17:50:48 +0700

    feat: register & login

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Users from './pages/Users';
 import Dashboard from './pages/Dashboard';
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 					<Route path='/' Component={Dashboard} />
 					<Route path='/users' Component={Users} />
 					<Route path='/register' Component={Register} />
+					<Route path='/login' Component={Login} />
 				</Routes>
 			</BrowserRouter>
 		</div>
---
+++ react-admin/src/pages/Login.tsx
@@ -0,0 +1,56 @@
+import axios from "axios";
+import { SyntheticEvent, useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+
+const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [redirect, setRedirect] = useState(false);
+
+    const submit = async (e: SyntheticEvent) => {
+        e.preventDefault();
+
+        const response = await axios.post('http://localhost:8000/api/login', {
+            email,
+            password
+        }, { withCredentials: true });
+
+        setRedirect(true);
+
+        console.log(response);
+    }
+
+    if (redirect) {
+        return <Navigate to="/" />
+    }
+
+    return (
+        <div className="container mt-5">
+            <h2>Login</h2>
+            <form onSubmit={submit}>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email</label>
+                    <input type="text" className="form-control" id="email"
+                        onChange={(e) => setEmail(e.target.value)} />
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="text" className="form-control" id="password"
+                        onChange={(e) => setPassword(e.target.value)} />
+                </div>
+
+                <button type="submit" className="btn btn-primary">Login</button>
+
+                <div className="mt-3">
+                    <span>Back to </span>
+                    <Link to="/register">Register</Link>
+                </div>
+
+            </form>
+        </div>
+    )
+}
+
+export default Login;
